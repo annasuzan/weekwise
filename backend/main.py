90f057986b75a951034b618950e6818cf993992d
annasuzan <46401107+annasuzan@users.noreply.github.com>
2026-03-21 23:44:18 -0400

    Fixing CORS error

--- backend/main.py
+++ backend/main.py
@@ -52,7 +52,7 @@ app.add_middleware(
         "http://localhost:3000", "http://127.0.0.1:3000",
         "http://localhost:5173", "http://127.0.0.1:5173",
         "http://localhost:8080", "http://127.0.0.1:8080",
-        "https://weekwise-tan.vercel.app/"
+        "https://weekwise-tan.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
